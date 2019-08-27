01756653613ccc5c92f9dfd5b2f8f972b8485c40
mani1soni <manish.soni8403@gmail.com>
2019-08-27 14:23:41 +0530

    notification file added

---
+++ vars/notification.groovy
@@ -0,0 +1,14 @@
+def call(){
+     post {
+        success {
+            emailext attachLog: true, 
+            body: '''success''', 
+            subject: 'build report', 
+            to: 'manish.soni@techifysolutions.com'
+        }
+        failure {
+            emailext attachLog: true, body: '''test failed and rollback initiated''', subject: 'jenkins build', to: 'manish.soni8403@gmail.com'
+        }
+    }
+}
+return this.&call()
