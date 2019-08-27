a3e4bb35b57eafd83585dc8d4388da43af03bd15
mani1soni <manish.soni8403@gmail.com>
2019-08-27 11:49:11 +0530

    groovy file updated

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
-#!/usr/bin/env groovy
 def call(CONTAINER,IMAGE){
    sh "sudo docker run -itd --name '${CONTAINER}' '${IMAGE}'"
 }
-return this
+return this.&call(CONTAINER,IMAGE)
 
